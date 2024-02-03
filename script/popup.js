3b194298faca2b11cb127882793ab9f248d8f935
WIZI <loick.konieczny@gmail.com>
2024-02-03 19:24:06 +0100

    add fix toggle button action

---
+++ script/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleCheckbox = document.getElementById('main-button')
+    
+    toggleCheckbox.addEventListener('change', function () {
+        chrome.runtime.sendMessage({ command: 'toggleExtension', value: toggleCheckbox.checked });
+    });
+      
+  });
--- script/service-worker.js
+++ script/service-worker.js
@@ -1,2 +1,22 @@
+const GREEN_COLOR = '#2db552';
+const GRAY_COLOR = '#828282';
+
+let extensionActive = true;
 chrome.action.setBadgeText({ text: 'ON' });
-chrome.action.setBadgeBackgroundColor({ color: '#06bf5f' });
+chrome.action.setBadgeBackgroundColor({ color: GREEN_COLOR });
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.command === 'toggleExtension') {
+        extensionActive = message.value;
+
+        if (extensionActive) {
+            console.log('Extension activée');
+            chrome.action.setBadgeText({ text: 'ON' });
+            chrome.action.setBadgeBackgroundColor({ color: GREEN_COLOR }); // Green color
+        } else {
+            console.log('Extension désactivée');
+            chrome.action.setBadgeText({ text: 'OFF' });
+            chrome.action.setBadgeBackgroundColor({ color: GRAY_COLOR }); // Gray color
+        }
+    }
+});
