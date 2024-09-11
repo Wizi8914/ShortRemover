51f3589e4bc0eef68258d6efc8d259a32775a4f4
Wizi8914 <loick.konieczny@gmail.com>
2024-09-12 00:10:27 +0200

    [hotfix] remove strict mod for options

--- scripts/translation.js
+++ scripts/translation.js
@@ -1,50 +1,43 @@
-"use strict";
-(() => {
-    const availableLanguages = ["en", "fr", "es", "de", "it", "ru", "ja", "ko", "sa", "zh"];
-    const defaultLanguage = navigator.language.split('-')[0];
-    
-    async function changeLanguage () {
-        const elements = document.querySelectorAll('[i18n-data]');
-        const language = await getLanguage();
-    
-        elements.forEach((element) => {
-            let messageKey = element.getAttribute('i18n-data'); 
-    
-            setMessage(element, messageKey, language);
+const availableLanguages = ["en", "fr", "es", "de", "it", "ru", "ja", "ko", "sa", "zh"];
+const defaultLanguage = navigator.language.split('-')[0];
+
+async function changeLanguage () {
+    const elements = document.querySelectorAll('[i18n-data]');
+    const language = await getLanguage();
+
+    elements.forEach((element) => {
+        let messageKey = element.getAttribute('i18n-data'); 
+
+        setMessage(element, messageKey, language);
+    });
+}
+
+function getLanguage() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get('language', function (result) {
+            let lang;
+            
+            if (availableLanguages.includes(defaultLanguage)) {
+                lang = result.language !== undefined ? result.language : defaultLanguage;
+            } else {
+                lang = result.language !== undefined ? result.language : 'en';
+            }
+
+            chrome.storage.local.set({ language: lang });
+            resolve(lang);
         });
-    }
-    
-    function getLanguage() {
-        return new Promise((resolve, reject) => {
-            chrome.storage.local.get('language', function (result) {
-                let lang;
-                
-                if (availableLanguages.includes(defaultLanguage)) {
-                    lang = result.language !== undefined ? result.language : defaultLanguage;
-                } else {
-                    lang = result.language !== undefined ? result.language : 'en';
-                }
-    
-                chrome.storage.local.set({ language: lang });
-                resolve(lang);
-            });
+    });
+}
+
+
+function setMessage(element, messageKey, language) {    
+    fetch(`../_locales/${language}/messages.json`)
+        .then((response) => response.json())
+        .then((json) => {
+
+            element.textContent = json[messageKey]['message'];
+        })
+        .catch((error) => {
+            console.error('Error:', error);
         });
-    }
-    
-    
-    function setMessage(element, messageKey, language) {    
-        fetch(`../_locales/${language}/messages.json`)
-            .then((response) => response.json())
-            .then((json) => {
-    
-                element.textContent = json[messageKey]['message'];
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
-})();
-
-
-
-    
+}
