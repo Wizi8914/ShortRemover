97ee840ccfb31c4695c2312c961829b51e70dae7
Wizi8914 <loick.konieczny@gmail.com>
2025-02-03 13:22:17 +0100

    Add Japanese and Chinese language support in options and set Text to english if is not translated

--- scripts/translation.js
+++ scripts/translation.js
@@ -1,5 +1,5 @@
 const defaultLanguage = navigator.language.split('-')[0];
-const availableLanguages = ["en", "fr", "ko", "ru", "de", "pt"];
+const availableLanguages = ["en", "fr", "ko", "ru", "de", "pt", "ja", "zh"];
 
 async function changeLanguage() {
     const elements = document.querySelectorAll('[i18n-data]');
@@ -52,8 +52,11 @@ function setMessage(element, messageKey, language) {
         .then((json) => {
             const lastParam = [...document.querySelectorAll('.param-name')].pop();
 
-            if (json[messageKey] == null && element == lastParam) fitTextSize(); // If the message is not found but it's the last parameter, fit the text size
-            if (json[messageKey] == null) return;
+            if (element == lastParam) fitTextSize(); // If the message is not found but it's the last parameter, fit the text size
+            
+            if (json[messageKey] == null) {
+                setMessage(element, messageKey, 'en');
+            };
 
             element.textContent = json[messageKey]['message'] !== null ? json[messageKey]['message'] : messageKey;
             
